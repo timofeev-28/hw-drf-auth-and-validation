e364f2a6f08bb0d6f0bc13df822a38ebfc5ef4fe
Dmitriy Timofeev <28dngn@gmail.com>
2026-06-16 22:04:14 +0300

    first commit

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
